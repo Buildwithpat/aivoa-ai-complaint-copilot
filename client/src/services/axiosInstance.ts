316e994c2b907dcf4041e501cea4cb2914bb231a
Buildwithpat <aakashbwp@gmail.com>
2026-07-28 17:34:04 +0530

    removed console

--- client/src/services/axiosInstance.ts
+++ client/src/services/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-console.log("API BASE URL:", import.meta.env.VITE_API_BASE_URL);
+;
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
